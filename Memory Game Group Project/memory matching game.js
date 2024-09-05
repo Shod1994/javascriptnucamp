a44e1dc72aa9bfee3f68a6f515b63f37c958dba6
Lashod Jones <lashodj@gmail.com>
2024-09-04 22:06:24 -0400

    updated solo Memory Card Game

---
+++ Memory Game Group Project/memory matching game.js
@@ -0,0 +1,11 @@
+Document.addEventListener('DOMLoadedContent', function () {
+    const cards = ["./Images/BlueEyesWhiteDragon.png", "./Images/DarkMagicianCard.png", "./Images/MonsterRebornCard.png", "./Images/PotOfGreedCard.png", "./Images/PotOfGreedCard.png", "RedEyesBlackDragonCard.png"
+        "./Images/BlueEyesWhiteDragon.png", "./Images/DarkMagicianCard.png", "./Images/MonsterRebornCard.png", "./Images/PotOfGreedCard.png", "./Images/PotOfGreedCard.png", "RedEyesBlackDragonCard.png"
+    ]
+    
+
+    // Shuffles Cards in Random Order
+    cards.sort(function(){
+        return 0.5 - Math.random();
+    })
+}
